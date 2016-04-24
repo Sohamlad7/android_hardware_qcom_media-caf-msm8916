d6c85b29aa7af7bb6d5bdfacfa2d65551e90c39e
Sohamlad7 <sohamlad7@gmail.com>
2016-04-24 18:03:45 +0530

    media-caf: mm-video-v4l2: point to av-caf
    Signed-off-by: Sohamlad7 <sohamlad7@gmail.com>

--- mm-video-v4l2/vidc/common/Android.mk
+++ mm-video-v4l2/vidc/common/Android.mk
@@ -28,7 +28,7 @@ libmm-vidc-inc      := $(LOCAL_PATH)/inc
 libmm-vidc-inc      += $(TOP)/$(call project-path-for,qcom-media)/mm-core/inc
 libmm-vidc-inc      += $(TARGET_OUT_HEADERS)/qcom/display
 libmm-vidc-inc      += $(TOP)/$(call project-path-for,qcom-media)/libc2dcolorconvert
-libmm-vidc-inc      += $(TOP)/frameworks/av/include/media/stagefright
+libmm-vidc-inc      += $(TOP)/frameworks/av-caf/include/media/stagefright
 libmm-vidc-inc      += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 
 LOCAL_ADDITIONAL_DEPENDENCIES   := $(libmm-vidc-add-dep)
